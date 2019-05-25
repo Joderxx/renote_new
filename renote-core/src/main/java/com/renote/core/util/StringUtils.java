55e7a34bd4d824f6d9688e3014f02b5dd78710e9
joder <974318723@qq.com>
2019-05-25 20:44:28 +0800

    update

--- renote-core/src/main/java/com/renote/core/util/StringUtils.java
+++ renote-core/src/main/java/com/renote/core/util/StringUtils.java
@@ -6,11 +6,14 @@ package com.renote.core.util;
  **/
 public class StringUtils {
 
-    public static boolean isEmpty(String str){
-        return str==null||"".equals(str.trim());
+    private StringUtils() {
     }
 
-    public static boolean isNotEmpty(String str){
+    public static boolean isEmpty(String str) {
+        return str == null || "".equals(str.trim());
+    }
+
+    public static boolean isNotEmpty(String str) {
         return !isEmpty(str);
     }
 }
