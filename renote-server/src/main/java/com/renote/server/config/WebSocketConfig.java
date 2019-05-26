4afb1a7d95582b2aa611764999f8d88428a5370f
joder <974318723@qq.com>
2019-05-26 19:30:39 +0800

    update

---
+++ renote-server/src/main/java/com/renote/server/config/WebSocketConfig.java
@@ -0,0 +1,18 @@
+package com.renote.server.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.server.standard.ServerEndpointExporter;
+
+/**
+ * @author joder
+ * @create 19-5-26
+ **/
+@Configuration
+public class WebSocketConfig {
+
+    @Bean
+    public ServerEndpointExporter serverEndpointExporter() {
+        return new ServerEndpointExporter();
+    }
+}
